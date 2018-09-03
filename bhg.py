1df8d42bbe62ed61cfd299425d0df9216dad4407
NinjaStyle <frank8290@gmail.com>
2018-09-03 13:21:17 -0600

    Added insecure flag

--- bhg.py
+++ bhg.py
@@ -4,7 +4,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 parser = argparse.ArgumentParser(description='Check headers for target web site.')
-parser.add_argument('-t','--target', dest='target', type=str, help='target')
+parser.add_argument('-t','--target', dest='target', type=str, help='Target')
+parser.add_argument('-i','--insecure', dest='verify', action="store_false", default=True, help='Make insecure requests')
 parser.add_argument('-u','--username', dest='username', type=str, required=False, help='Username for HTTP Basic authentication')
 parser.add_argument('-p','--password', dest='password', type=str, required=False, help='Passsword for HTTP Basic authentication')
 args = parser.parse_args()
@@ -14,11 +15,11 @@ def CheckHeader(headers,search):
         return search+": "+headers[search]
     return False
 
-def RetrieveHeaders(target, username, password):
+def RetrieveHeaders(target, verify, username, password):
     if username and password:
-        headers = requests.get(target, auth=HTTPBasicAuth(username, password)).headers
+        headers = requests.get(target, auth=HTTPBasicAuth(username, password), verify=verify).headers
     else:
-		headers = requests.get(target).headers
+	headers = requests.get(target, verify=verify).headers
     return headers
     
 searchlist = [\
@@ -30,9 +31,9 @@ searchlist = [\
 ]
 try:
 	if args.username and args.password:
-		 headers = RetrieveHeaders(args.target, args.username, args.password)
+		headers = RetrieveHeaders(args.target, args.verify, args.username, args.password)
 	else:
-		headers = RetrieveHeaders(args.target, False, False)
+		headers = RetrieveHeaders(args.target, args.verify, False, False)
 	print "Getting Headers for: "+args.target+"\n"
 except Exception as e:
     print e
